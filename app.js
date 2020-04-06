3e809e3e045227ef9efdb10e8eeade754cf45f6f
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-04-06 14:45:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,10 @@
-let board;
-document.getElementById('board').addEventListener('click',handleTurn);
- {
-   let idx = squares.findIndex(function(square) {
-return square === event.target;
-    });
-};
-function handleTurn(event) {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target;
-    });
-    board [idx] = turn;
-    console.log(board)
-};
-
-function render() {
-        board.forEach(function(mark, index) {
-        console.log(mark, index);
-        });
-        };
-        function init() {
-            board = [
-            '', '', '',
-            '', '', '',
-            '', '', ''
-            ];
-            render();
-            };
-            const squares = Array.from
-            (document.querySelectorAll('#board div'));
-            function render() {
-                board.forEach(function(mark, index) {
-                    squares[index].textContent = mark;
-                });
-            };
-
-function handleTurn() {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target;
-    });
-    board[idx] = turn;
-
-    if (turn === 'X') {
-   turn = 'O' 
-   } else {
-   turn = 'X' 
-   };
- 
-
-const messages = document.querySelector('h2');
-function render () {
-    board.forEach(function(val, idx) {
-        squares[idx].textContent = val;
-    });
-    messages. textContent = `${turn} turn!`;
-};
-
-let win;
-win = board[0] && board[0] === board[1] && board [0] 
-=== board[2] ?
-board[0] : null;
-const winningCombos = [
+var lookup = {
+    '1': 'red',
+    '-1': 'blue',
+    'null': 'white'
+  };
+  
+  var winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -68,38 +13,60 @@ const winningCombos = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-];
+  ];
+  
+  
+  var board, turn, winner
+  
 
+  var squares = document.querySelectorAll('td div');
+  var message = document.querySelector('h2');
+  
 
-function handleTurn() {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target;
-    });
+  document.querySelector('table').addEventListener('click', handleMove);
+  document.querySelector('button').addEventListener('click', initialize);
+  
+  
+  initialize();
+  
+  function handleMove(evt) {
+    
+    var idx = parseInt(evt.target.id.replace('sq', ''));
+ 
+    if (board[idx] || winner) return
     board[idx] = turn;
-    win = getWinner();
+    turn *= -1;
+    winner = getWinner();
     render();
-};
-function getWinner() {
-    let winner = null;
-    winningCombos.forEach(function(combo, index) {
-        if (board[combo[0]] && board [combo[0]] === board[combo[1]]
-             && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
-           });
-           return winner;
-        };
-        messages.textContent = win ? `${win} 
-        wins the game!` : ` ${turn} turn!`; 
-        function getWinner() {
-            let winner = null;
-            winningCombos.forEach((combo, index) => {
-             if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
-           winner = board[combo[0]];
-         }
-             });           return winner ? winner : board.includes('') ? null : 'T';
- }; 
+  }
+  
+  function getWinner() {
+    for (var i = 0; i < winningCombos.length; i++) {
+      if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + 
+        board[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
+    }
+    
+    if (board.includes(null)) return null;
+    return 'T';
+  }
+  
+  function render() {
+    board.forEach(function(sq, idx) {
+      squares[idx].style.background = lookup[sq];
+    });
+    if (winner === 'T') {
+      message.innerHTML = 'Oops, another tie!';
+    } else if (winner) {
+      message.innerHTML = `Congrats ${lookup[winner].toUpperCase()}!`;
+    } else {
+      message.innerHTML = `${lookup[turn].toUpperCase()}'s Turn`;
+    }
+  }
+  
+  function initialize() {
+    board = [null, null, null, null, null, null, null, null, null];
 
- messages.textContent = win === 'T' ? `That's a tie!` :/  win ? `${win} wins the game!` : ` ${turn} turn!`;/  document.getElementById('board').addEventListener
-  ('click', handleMove);
-
-  document.getElementById('reset-button').addEventListener
- ('click', init);
+    turn = 1;
+    winner = null;
+    render();
+  }
